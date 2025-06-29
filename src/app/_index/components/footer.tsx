647c350cb674cd0fdec4e9a4f13474fd9297f359
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-29 00:45:10 -0500

    feat: added footer for welcome page.

---
+++ src/app/_index/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="text-center text-sm text-gray-500">
+      © 2025 BonoPlan. Todos los derechos reservados.
+    </footer>
+  );
+};
