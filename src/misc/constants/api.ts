7c6e1331bf9881f786362d3c39647d25d58cb1c7
Mauricio <mauriciorojasanchez@gmail.com>
2025-07-07 00:08:40 -0500

    feat: added api misc constants..

---
+++ src/misc/constants/api.ts
@@ -0,0 +1,11 @@
+
+export const BASE_API_URL = ""
+
+export const HEADERS = {
+    CONTENT_TYPE: 'Content-Type',
+    APPLICATION_JSON: 'application/json',
+    AUTHORIZATION: 'Authorization',
+    BEARER_TOKEN: 'Bearer '
+}
+
+export const API_TIMEOUT = 30
